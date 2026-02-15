37c7658fec0bafc9c85d6613b29ca744149ccd0a
Kenza69420 <john.knezu@gmail.com>
2026-02-15 21:20:13 +0100

    fetch

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
-// api.js - Handles fetching advice from the API
-
 const API_URL = 'https://api.adviceslip.com/advice';
+
+export async function fetchAdvice() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data.slip;
+}
