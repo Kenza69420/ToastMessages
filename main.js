4aa568e95c1947d9b2bac628e8e620993968d546
Kenza69420 <john.knezu@gmail.com>
2026-02-15 21:28:23 +0100

    generateNotification

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import { fetchAdvice } from './api.js';
 import { showToast } from './ui.js';
+
+async function generateNotification() {
+    let data = await fetchAdvice();
+    showToast(data.advice);
+}
+
+generateNotification();
