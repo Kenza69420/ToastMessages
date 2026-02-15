5b50a9d4a84a59634dc9604a146bbea32d924d0a
Kenza69420 <john.knezu@gmail.com>
2026-02-15 21:35:05 +0100

    kaše

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 import { fetchAdvice } from './api.js';
 import { showToast } from './ui.js';
 
+let cache = [];
+let counter = 0;
+
 async function generateNotification() {
-    let data = await fetchAdvice();
-    showToast(data.advice);
+    counter++;
+
+    if (counter % 3 == 0 && cache.length > 0) {
+        let random = Math.floor(Math.random() * cache.length);
+        showToast(cache[random], true);
+    } else {
+        let data = await fetchAdvice();
+        cache.push(data.advice);
+        showToast(data.advice, false);
+    }
 }
 
 generateNotification();
